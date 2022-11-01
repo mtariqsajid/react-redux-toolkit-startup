3a9399f99dd76dbb16cb6b9a455d38660e34c858
Tariq Sajid <mtariqsajid@gmail.com>
2022-11-01 18:58:32 +0000

    Update 10 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./styles.css";
+import React, { lazy } from "react";
+
+const Home = lazy(() => import("./pages/home/Home"));
+const Post = lazy(() => import("./pages/posts/Post"));
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="post" element={<Post />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/posts/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Post.css";
+
+function Post() {
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+    </div>
+  );
+}
+
+export default Post;
